0c7d7441b789a177189425f426c35fe7f7474dea
Michael De Luca <michael.deluca@circle-free.com>
2025-07-31 06:30:35 -0400

    feat: app-chain

---
+++ app-chain/src/group-message-broadcaster.ts
@@ -0,0 +1,6 @@
+import {} from '@graphprotocol/graph-ts';
+
+/* ============ Handlers ============ */
+
+// For now, this is too intensive on USDC, but will be feasible on FeeToken/xUSD.
+export function handleTransfer(event: void): void {}
